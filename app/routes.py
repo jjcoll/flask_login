04aab3e7df888e54776e5f453ded5078386a883a
jjcoll <jjordicoll@gmail.com>
2024-03-06 13:04:04 +0100

    added basic file structure and basic configuration

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from app import app
+from flask import render_template
+
+
+@app.route("/")
+@app.route("/home")
+def home_page():
+    return render_template("index.html")
+
+
+@app.route("/register")
+def register_page():
+    return render_template("register.html")
+
+
+@app.route("/login")
+def login_page():
+    return render_template("login.html")
